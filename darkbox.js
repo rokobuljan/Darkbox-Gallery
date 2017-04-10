5dfbb8d2a434c107e6e6f5d355fa2fcbb6911626
Roko C. Buljan <roko.cb@gmail.com>
2017-04-10 06:39:59 +0200

    Update darkbox.js
    Improved JS + update with group feature. next/prev now toggle. stats counter added

--- darkbox.js
+++ darkbox.js
@@ -1,74 +1,115 @@
 // /////
 // Darkbox by Roko.CB
+// https://github.com/rokobuljan/Darkbox-Gallery
 (function () {
 
-    var c = 0; // counter
-    var $images = {};
-    var n = 0;
-    var $prevNext = $("<div id='darkbox_prev'/><div id='darkbox_next'/>").on("click", function (e) {
-        e.stopPropagation();
-        var isNext = this.id === "darkbox_next";
-        c = (isNext ? c++ : --c) < 0 ? n - 1 : c % n;
-        $images.eq(c).click();
-    });
-    var $darkboxDescription = $("<div/>", {
-        "id": "darkbox-description"
-    });
-    var $darkbox = $("<div/>", {
-        "id": "darkbox",
-        html: $prevNext
-    }).on("click", function () {
-        $(this).removeClass("on");
-    }).append($darkboxDescription).appendTo("body");
-
-    $(document).on("click", "[data-darkbox]", function (e) {
-
+  var $imagesGroup = $(),
+      n = 0, // number of images
+      c = 0, // current image index (counter 0-based)
+      $prevNext = $("<a id='darkbox_prev'/><a id='darkbox_next'/>").on("touchstart mousedown", function (e) {
         e.preventDefault();
         e.stopPropagation();
+        var isNext = this.id === "darkbox_next";
+        c = isNext ? ++c : --c;
+        showImage();
+      }),
+      $darkbox = $("<div/>", {
+        id: "darkbox",
+        append: $prevNext,
+        appendTo : "body"
+      }),
+      $darkboxImg = $("<img/>", {
+        id: "darkbox_img",
+        appendTo: $darkbox,
+        on :{
+          click : function( e ) {
+            e.stopPropagation();
+          }
+        }
+      }),
+      $darkboxClose = $("<a/>", {
+        id: "darkbox_close",
+        appendTo: $darkbox,
+        on: {
+          "touchstart mousedown" : function(e) {
+            e.preventDefault();
+            $darkbox.removeClass("show");
+          }
+        }
+      }),
+      $darkboxDescription = $("<div/>", {
+        id: "darkbox_description",
+        appendTo : $darkbox
+      }),
+      $darkboxStats = $("<div/>", {
+        id: "darkbox_stats",
+        appendTo : $darkbox
+      });
 
-        $images = $('[data-darkbox!=""][data-darkbox]');
-        n = $images.length;
 
-        var that = this;
-        var docW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        var docH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        var o = this.getBoundingClientRect();
-        var imgSRC = this.dataset.darkbox ? this.dataset.darkbox : this.src;
-        if (!imgSRC)
-            imgSRC = $("[data-darkbox!=''][data-darkbox]").attr("data-darkbox");
-        var newImg = new Image();
-        newImg.onload = function () {
-            c = $images.index(that);
-            var bigger = (newImg.height > docH || newImg.width > docW);
-            $darkbox.css({// Init darkbox to image position
-                backfaceVisibility: "hidden",
-                transition: "0s",
-                backgroundImage: "url('" + newImg.src + "')",
-                backgroundSize: bigger ? "contain" : "auto",
-                left: o.left,
-                top: o.top,
-                height: that.height,
-                width: that.width
-            });
-            $darkboxDescription.html($(that).attr("data-darkbox-description"));
-            setTimeout(function () {
-                if (!$darkbox.hasClass("on")) {
-                    $darkbox.css({transition: "0.8s"}).addClass("on");
-                }
-            }, 35);
-        };
-        newImg.src = imgSRC;
+  
+  function showImage() {
 
-    });
+    // Prevent counter going out of bounds
+    c = c < 0 ? n - 1 : c % n;
 
-    $(document).on("keyup", function (e) {
-        var k = e.which;
-        if (k === 27) /*ESC */
-            $("#darkbox").click(); // close Darkbox
-        if (k === 37) /*LEFT*/
-            $("#darkbox_prev").click();
-        if (k === 39) /*RIGHT*/
-            $("#darkbox_next").click();
-    });
+    // Get size of window so that we can define if
+    // background-size needs to be "contain" or "auto".
+    var docW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+        docH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+        $cur = $imagesGroup.eq(c),
+        description = $cur.data("darkbox-description"),
+        src = $cur.data("darkbox");
+
+    $darkbox.addClass("show spinner");
+    $darkboxDescription.html(description);
+    $darkboxStats.html(n<2?"" : (c+1) +"/"+ n);
+
+    $("<img/>").on("load", function(){
+      var bigger = (this.width > docW || this.height > docH);
+      $darkbox.removeClass("spinner").css({
+        backgroundImage: "url('" + this.src + "')",
+        backgroundSize: bigger ? "contain" : "auto"
+      });
+    }).attr("src", src);
+
+  }
+
+  
+  // Call darkbox
+  $(document).on("click", "[data-darkbox],[data-darkbox-group]", function (e) {
+
+    var src = $(this).data("darkbox"),
+        isDummy = !src, // (is just a link calling a group)
+        groupID = $(this).data("darkbox-group");
+
+    $imagesGroup = !groupID ? $(this) : $('[data-darkbox-group="'+ groupID +'"]').filter("[data-darkbox]")
+    n = $imagesGroup.length;
+    c = isDummy ? 0 : $imagesGroup.index( this );
+
+    $prevNext.toggle(n>1); // Hide prev/next if we have a single image
+    $darkbox.css({backgroundImage:"none"});
+
+    showImage(); // aand, ACTION!
+  });
+
+  
+  // Keyboard navigation
+  $(document).on("keyup", function (e) {
+    
+    var k = e.which;
+    
+    if (k === 27) /*ESC */ {
+      $darkbox.removeClass("show");
+    }
+    if (k === 37) /*LEFT*/ {
+      --c;
+      showImage();
+    }
+    if (k === 39) /*RIGHT*/ {
+      ++c;
+      showImage();
+    }
+  });
 
 }());
